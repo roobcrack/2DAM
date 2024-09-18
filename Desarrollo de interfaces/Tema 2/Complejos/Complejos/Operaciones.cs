a811f77a06a60db00e74266b6b993132c98b0110
roobcrack <39882967+roobcrack@users.noreply.github.com>
2024-09-18 20:32:48 +0200

    Add

---
+++ Desarrollo de interfaces/Tema 2/Complejos/Complejos/Operaciones.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Complejos
+{
+    private double real { get; set; }
+    private double imaginario { get; set; }
+
+    public Complejos() { }
+    public Complejos(double real, double imaginario)
+    {
+        this.real = real;
+        this.imaginario = imaginario;
+    }
+
+    //Suma de 2 imaginarios
+    public static Complejos operator + (Complejos comp1, Complejos comp2)
+    {
+        Complejos op = new Complejos(comp1.real+comp2.real, comp1.imaginario+comp2.imaginario);
+        return op;
+    }
+
+    //Resta de 2 imaginarios
+    public static Complejos operator - (Complejos comp1, Complejos comp2)
+    {
+        Complejos op = new Complejos(comp1.real - comp2.real, comp1.imaginario - comp2.imaginario);
+        return op;
+    }
+
+    //Multiplicacion de 2 imaginarios
+    public static Complejos operator * (Complejos comp1, Complejos comp2)
+    {
+        Complejos op = new Complejos((comp1.real * comp2.real) - (comp1.imaginario * comp2.imaginario),
+            ((comp1.real * comp2.imaginario) + (comp1.imaginario * comp2.real)));
+        return op;
+    }
+
+    //Multiplicacion de 2 imaginarios
+    public static Complejos operator * (double num, Complejos comp)
+    {
+        Complejos op = new Complejos(num*comp.real, num*comp.imaginario);
+        return op;
+    }
+
+    //ToString que muestra el numero completo
+    public string ToString()
+    {
+        return real + "+" + imaginario + "i";
+    }
+}
---
+++ Desarrollo de interfaces/Tema 2/Complejos/Complejos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Complejos complejo1 = new Complejos(5, 6);
+        Complejos complejo2 = new Complejos(8, 2);
+        Complejos resultado;
+
+        //suma
+        resultado = complejo1 + complejo2;
+        Console.WriteLine(complejo1.ToString() + " suma " + complejo2.ToString() + " = " + resultado.ToString());
+        //resta
+        resultado = complejo1 - complejo2;
+        Console.WriteLine(complejo1.ToString() + " resta " + complejo2.ToString() + " = " + resultado.ToString());
+        //multiplicacion
+        resultado = complejo1 * complejo2;
+        Console.WriteLine(complejo1.ToString() + " multiplicacion " + complejo2.ToString() + " = " + resultado.ToString());
+        //multiplicacion por real
+        resultado = 7.5 * complejo2;
+        Console.WriteLine("7.5 multiplicacion " + complejo2.ToString() + " = " + resultado.ToString());
+    }
+}
