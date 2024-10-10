387c173bb70d942e8cd0570e3243ae87f4d52156
roobcrack <39882967+roobcrack@users.noreply.github.com>
2024-10-10 15:18:46 +0200

    Add

--- Acceso a datos/eclipse-workspace/ProgramacionFuncional/src/main/java/com/ruben/ProgramacionFuncional/AppEntry.java
+++
@@ -1,9 +0,0 @@
-package com.ruben.ProgramacionFuncional;
-
-import java.util.ArrayList;
-import java.util.list;
-
-import com.ruben.ProgramacionFuncional.entidades.copy;
-public class AppEntry {
-    }
-}
